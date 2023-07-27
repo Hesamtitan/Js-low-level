48daaf527c547701d789459e34a9af7f32e341bb
HesaM <hesam.titan.666@gmail.com>
2023-07-28 00:28:38 +0330

    stat

---
+++ main.js
@@ -0,0 +1,6 @@
+const name = prompt("enter you name");
+let age = 34
+let isAdult = true
+console.log("Wellcome" + " " + name);
+console.log("Wellcome" + " " + name);
+console.log("Wellcome" + " " + name);
