d8d3b17488d82728e0d3dfd5def353e850b54bb0
HesaM <hesam.titan.666@gmail.com>
2023-07-28 03:35:08 +0330

    2

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const name = prompt("enter you name");
-let age = prompt("enter your age");
-let isAdult = true;
-console.log("Wellcome" + " " + name);
-console.log("your age" + " " + age);
-console.log(isAdult);
+// let name = prompt("enter you name");
+// let age = prompt("enter your age");
+// let isAdult = true;
+// console.log("Wellcome" + " " + name);
+// console.log("your age" + " " + age);
+// console.log(isAdult);
+
+//_____________________________________________________________________
+
+let firstName = prompt();
+if (firstName == "hesam") {
+  alert("Wellcome hesam");
+}
